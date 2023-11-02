2c70e47eaeacfd89afc1259a110f254ea4379cd4
alberdidavid <alberdi11david@gmail.com>
2023-11-02 18:58:29 -0300

    Creacion del Repositorio

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <div className="flex flex-row items-center p-4">
+        <img src="" alt="" />
+        <div className="">
+          <a href="" className="font-ub">Quien Soy?</a>
+          <a href="">Blog</a>
+          <a href="">Contactanos</a>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
